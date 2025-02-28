4d602ad7d0f8ae57d19e18c5bd01ae2d937b5f3c
Nethan Nagendran <nethan.nagendran@gmail.com>
2025-02-28 18:05:13 -0500

    removed comments for java

--- src/components/Projects-Components/computer-science/GithubProjects.js
+++ src/components/Projects-Components/computer-science/GithubProjects.js
@@ -28,7 +28,7 @@ export default function GithubProjects() {
                 <div className='Github-Grid'>
                     <PythongitCard gitName="GTMovieStore" description="Programmed in Django for Objects & Design class @GT with group" Git_Link="https://github.com/nakulshah04/GTMovieStore" />
                     <PythongitCard gitName="googleForm_Requests" description="Automate Google Forms by sending multiple Requests" Git_Link="https://github.com/nethann/googleForm_Requests/tree/main" />
-                    <JavaScriptgitCard gitName="Java" description="Discord bot that helps moderate  Discord Servers" Git_Link="https://google.com" />
+                    {/* <JavaScriptgitCard gitName="Java" description="Discord bot that helps moderate  Discord Servers" Git_Link="https://google.com" /> */}
 
                     <PythongitCard gitName="MiniBox" description="Graphical User Interface made using Tkinter from Python." Git_Link="https://github.com/nethann/Minibox" />
                 </div>
