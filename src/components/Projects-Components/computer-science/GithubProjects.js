77039bfe9f919f29febce68eb422093e150c0ce6
Nethan Nagendran <nethan.nagendran@gmail.com>
2025-06-30 12:39:27 -0400

    updated git rep height issue

--- src/components/Projects-Components/computer-science/GithubProjects.js
+++ src/components/Projects-Components/computer-science/GithubProjects.js
@@ -28,6 +28,7 @@ export default function GithubProjects() {
                 <div className='Github-Grid'>
                     <PythongitCard gitName="GTWalkThrough" description="GTWalkThrough is a community-powered web app designed to help Georgia Tech students and staff navigate campus more efficiently by avoiding active construction zones." Git_Link="https://github.com/nakulshah04/GTWalkThrough" /> 
                     <PythongitCard gitName="GTMovieStore" description="GTMovieStore is a web application that allows users to browse, review, and purchase movies with a seamless shopping cart experience." Git_Link="https://github.com/nakulshah04/GTMovieStore" />
+                    <PythongitCard gitName="betterCanvas" description="A simple, interactive Tkinter-based GUI that connects to your Georgia Tech Canvas account and displays all upcoming assignments (due today or later) for your current active courses." Git_Link="https://github.com/nethann/betterCanvas" />
                     <PythongitCard gitName="googleForm_Requests" description="Automate Google Forms by sending multiple Requests" Git_Link="https://github.com/nethann/googleForm_Requests/tree/main" />
                     {/* <JavaScriptgitCard gitName="Java" description="Discord bot that helps moderate  Discord Servers" Git_Link="https://google.com" /> */}
                     <PythongitCard gitName="MiniBox" description="Graphical User Interface made using Tkinter from Python." Git_Link="https://github.com/nethann/Minibox" />
