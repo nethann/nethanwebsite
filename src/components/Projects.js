9256a4703758c26bfc28ea9886520ebc05001e0b
Nethan Nagendran <nethan.nagendran@gmail.com>
2025-10-10 23:24:06 -0400

    updated programmed with react to uppsercase

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -632,12 +632,12 @@ function CompletedProjects() {
       <p className='Git-Title'>Engineering Journey 🍏 : </p>
 
       <div className='CompSciProjects_Grid'>
-        <CompletedProjectCard img1={LightTriangle_1} img2={LightTriangle_2} title="Pyramid Lights" description="Lazer cut a pyramid shape and programmed 3 light patterns to function" date="2020" />
+        <CompletedProjectCard img1={LightTriangle_1} img2={LightTriangle_2} title="Pyramid Lights" description="Lazer cut a pyramid shape and Programmed 3 light patterns to function" date="2020" />
         <CompletedProjectswithVid vid={Gears_Spinning} img1={Gears_Pic} title="Gear Kinetic Sculpture" description="Prototyped gears and acrylic using Inkscape & made gears which spins opposite directions and normal direction. " date="2021" />
-        <CompletedProjectswithVid vid={Arduino_Motor} img1={Arduino_MotorCircuit} title="Arduino Motor Prototyping" description="Video of motor spinning for the distance sensing robot and the circuit diagram. I programmed two motors to spin with speed input. This was progress on starting the main distance sensing robot. " date="2021" />
+        <CompletedProjectswithVid vid={Arduino_Motor} img1={Arduino_MotorCircuit} title="Arduino Motor Prototyping" description="Video of motor spinning for the distance sensing robot and the circuit diagram. I Programmed two motors to spin with speed input. This was progress on starting the main distance sensing robot. " date="2021" />
         <CompletedProjectswithVid vid={CardboardMotor} img1={CardBoardMotor_Setup} title="Arduino Motor Prototyping with cardboard" description="Programmed the ultrasonic sensor to read distance and connected batteries to power the arduino using cardboard as the body found at home." date="2021" />
-        <CompletedProjectswithVid vid={Distance_Robot} img1={DistanceRobot_Circuit} title="Distance Sensing Robot" description="Prototyped and programmed a distance sensing robot which can be programmed for directions. While moving, it will continue to read distance to see if there are any objecting blocking. If so, it will turn either left or right before the robot is hit to the object. " date="2021" />
-        <CompletedProjectCard img1={GoogleAIY_Front} img2={GoogleAIY_Back} title="Google AIY" description="Built and programmed the Google AIY kit to detect face emotion where the button will change color depending on the face the user makes." date="2022" />
+        <CompletedProjectswithVid vid={Distance_Robot} img1={DistanceRobot_Circuit} title="Distance Sensing Robot" description="Prototyped and Programmed a distance sensing robot which can be Programmed for directions. While moving, it will continue to read distance to see if there are any objecting blocking. If so, it will turn either left or right before the robot is hit to the object. " date="2021" />
+        <CompletedProjectCard img1={GoogleAIY_Front} img2={GoogleAIY_Back} title="Google AIY" description="Built and Programmed the Google AIY kit to detect face emotion where the button will change color depending on the face the user makes." date="2022" />
       </div>
     </section>
   )
